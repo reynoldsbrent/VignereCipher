0edc88f66beb767b02f2bba31019a12e12c57adc
Brent Reynolds <brentsreynolds5@gmail.com>
2023-08-16 17:19:24 -0400

    Added comments and updated Readme.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# VignereCipher
+# Vignere Encryption
+
+This Python script performs keyword encryption on a given text using a user-provided keyword. It shifts each letter of the text based on the corresponding letter's position in the keyword. The resulting encrypted text is displayed as output.
+
+## How the Encryption Works
+
+- The script takes a clear text and a keyword from the user.
+- It creates lists to store individual letters of the text, the keyword, and the encrypted text.
+- For each letter in the text, the script finds the corresponding letter from the keyword based on position.
+- Shift values are calculated for each keyword letter using the alphabet index.
+- The script encrypts each letter of the text using the calculated shifts.
+- The encrypted letters are joined to form the encrypted string.
+
+## Example Output:
+
+Which text should be encrypted: secretmessage
+Which keyword should be used: keyword
+cianskpowqwuv
+
+## How to Use
+
+1. Save the provided code to a file, e.g., `keyword_encryption.py`.
+
+2. Open a terminal or command prompt.
+
+3. Navigate to the directory where the script is located.
+
+4. Run the script using the following command:
+
+   ```bash
+   python keyword_encryption.py
+Enter the text you want to encrypt when prompted.
+
+Enter the keyword you want to use for encryption.
+
+The script will perform keyword encryption and display the resulting encrypted text.
--- exercise.py
+++ exercise.py
@@ -1,24 +1,38 @@
 import math
 
+# Get the input text to be encrypted from the user
 clear_text = input("Which text should be encrypted: ")
+# Get the keyword to be used for encryption from the user
 keyword = input("Which keyword should be used: ")
+
+# Lists to store individual letters of the text and keyword
 word_list = []
 keyword_list = []
+# List to store the keyword letters that correspond to each letter in the text
 keys = []
+# List to store the encrypted letters
 encrypted_text = []
+
+# List of alphabet letters
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# List to store calculated shifts for each letter in the text
 shifts = []
+
+# Function to encrypt the text using the keyword
 def encrypt_text(text, key_word):
     encrypted_string = ""
 
+    # Populate the word_list with individual letters of the text
     for letter in text.lower():
         word_list.append(letter)
     
+    # Populate the keyword_list with individual letters of the keyword
     for character in key_word.lower():
         keyword_list.append(character)
 
     count = 0
     letter_count = 0
+    # Match each letter of the text with a letter from the keyword
     while count < len(word_list):
         if letter_count == len(keyword_list):
             letter_count = 0
@@ -28,24 +42,26 @@ def encrypt_text(text, key_word):
         count += 1
         letter_count += 1
     
+    # Calculate shifts for each keyword letter
     for key in keys:
        shift = calculate_shifts(key)
        shifts.append(shift)
     count = 0
+    # Encrypt each letter of the text
     for letter in word_list:
         if word_list.index(letter) == len(shifts):
             count = 0
-        #encrypted = encrypt_letter(letter, shifts[word_list.index(letter)])
         encrypted = encrypt_letter(letter, shifts[count])
         encrypted_text.append(encrypted)
         count += 1
     
-    
+    # Build the encrypted string from the list of encrypted letters
     for letter in encrypted_text:
         encrypted_string = encrypted_string + f"{letter}"
     
     return encrypted_string
 
+# Function to encrypt a single letter using a shift value
 def encrypt_letter(character, shift):
     letter = ''
     if character not in alphabet:
@@ -56,10 +72,10 @@ def encrypt_letter(character, shift):
         letter = alphabet[(alphabet.index(character) + shift) - 26]
     return letter
 
-
+# Function to calculate the shift value based on a keyword letter
 def calculate_shifts(letter):
     shift = alphabet.index(letter)
     return shift
 
-
+# Call the encrypt_text function and print the encrypted result
 print(encrypt_text(clear_text, keyword))
